52d987b7a658f5eb05c1742438b06dcbf8e834d4
DanSmithEKM <dansmith@ekmsystems.co.uk>
2017-04-18 14:46:13 +0100

    Added in a height check
    The show/hide functionality may not be appropriate for all descriptions as they may not be sufficiently long enough to benefit from it.

--- mobile-incat-showhide.js
+++ mobile-incat-showhide.js
@@ -1,25 +1,35 @@
 document.getElementById("show-more").addEventListener("click", expand);
-		document.getElementById("show-less").addEventListener("click", collapse);
+document.getElementById("show-less").addEventListener("click", collapse);
 
-		function expand() {
-    		$(".incategory-description").removeClass("collapsed");
-    		$(".incategory-description").addClass("expanded");
-    		$(".incategory-description").css("max-height", "2000px");
-    		$("#show-more").removeClass("show");
-    		$("#show-more").addClass("hide");
-    		$("#show-less").removeClass("hide");
-    		$("#show-less").addClass("show")
-    		$("#controls").removeClass("fade");
-		}
+function expand() {
+	$(".incategory-description").removeClass("collapsed");
+    	$(".incategory-description").addClass("expanded");
+    	$(".incategory-description").css("max-height", "2000px");
+    	$("#show-more").removeClass("show");
+    	$("#show-more").addClass("hide");
+    	$("#show-less").removeClass("hide");
+    	$("#show-less").addClass("show")
+    	$("#controls").removeClass("fade");
+}
 
-		function collapse() {
-    		$(".incategory-description").removeClass("expanded");
-    		$(".incategory-description").addClass("collapsed");
-    		$(".incategory-description").css("max-height", "180px");
-    		$("#show-more").removeClass("hide");
-    		$("#show-more").addClass("show");
-    		$("#show-less").addClass("hide");
-    		$("#show-less").removeClass("show");
-    		$("#controls").addClass("fade");
-		}
-	
+function collapse() {
+	$(".incategory-description").removeClass("expanded");
+    	$(".incategory-description").addClass("collapsed");
+    	$(".incategory-description").css("max-height", "180px");
+    	$("#show-more").removeClass("hide");
+    	$("#show-more").addClass("show");
+    	$("#show-less").addClass("hide");
+    	$("#show-less").removeClass("show");
+    	$("#controls").addClass("fade");
+}
+		
+function checkHeight() {
+	var incatDesc = $(".incategory-description");
+	if (incatDesc.height() < 100) {
+		$("#show-more").removeClass("show");
+		$("#show-more").addClass("hide");
+		$(".controls").removeClass("show");
+		$(".controls").addClass("hide");
+	}
+}
+checkHeight();
